347b1227116d8b7c737785c3879f70bc74f18a20
Justmaina <githuamaina10@gmail.com>
2023-03-14 18:12:08 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,39 +6,25 @@
  * @str: char
  * Return: 0
  */
-
-char *_strdup(char *str)		
+char *_strdup(char *str)
 {
-
 	char *aaa;
-
 	int i, r = 0;
 
-
-
 	if (str == NULL)
-
 		return (NULL);
-
 	i = 0;
-
 	while (str[i] != '\0')
-
 		i++;
-
 
 	aaa = malloc(sizeof(char) * (i + 1));
 
-
-	if (av == NULL)
-
+	if (aaa == NULL)
 		return (NULL);
 
 	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
 
-		av[r] = str[r];
-
-
-	return (av);
-
+	return (aaa);
 }
+
